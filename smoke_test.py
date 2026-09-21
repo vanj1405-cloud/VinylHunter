65da5de4539ddb0c6d998734d079f3d04bda3957
vanj1405-cloud <vanj1405@gmail.com>
2026-09-21 18:38:29 +0300

    Delete smoke_test.py

--- smoke_test.py
+++
@@ -1,7 +0,0 @@
-import core
-from providers.marketplaces import credential_status
-
-print('Israeli stores:', len(core.store_registry()))
-print('International candidates:', len(core.INTERNATIONAL_REGISTRY))
-print('Marketplace credentials:', credential_status())
-print('OK')
